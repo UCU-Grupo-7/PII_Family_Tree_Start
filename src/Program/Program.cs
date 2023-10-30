d9ee7021384e9debadb7337370cee9a0b9f5e423
nanosanchez22 <nanosanchez22@gmail.com>
2023-10-30 10:50:20 -0300

    parte 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,6 +26,8 @@ namespace Program
 
             // visitar el árbol aquí
 
+            /* 
+            // Parte 1
             Person person1 = new Person("Juan", 60);
             Person person2 = new Person("Jose", 24);
             Person person3 = new Person("Josefina", 18);
@@ -35,7 +37,51 @@ namespace Program
             Node<Person> node3 = new Node<Person>(person3);
 
             node1.AddChildren(node2);
-            node1.AddChildren(node3);
+            node1.AddChildren(node3); 
+            */
+
+            //Parte 2
+
+            Person abueloPadre = new Person("Julio", 85);
+            Person abuelaPadre = new Person("Margarita", 75);
+            Person abueloMadre = new Person("Ramon", 80);
+            Person abuelaMadre = new Person("Marta", 72);
+            Person padre = new Person("Roberto", 58);
+            Person madre = new Person("Maria", 49);
+            Person hijo1 = new Person("Belen", 16);
+            Person hijo2 = new Person("Matias", 22);
+            Person hijo3 = new Person("Luis", 28);
+
+            Node<Person> nodeAbueloP = new Node<Person>(abueloPadre);
+            Node<Person> nodeAbuelaP = new Node<Person>(abuelaPadre);
+
+            Node<Person> nodeAbueloM = new Node<Person>(abueloMadre);
+            Node<Person> nodeAbuelaM = new Node<Person>(abuelaMadre);
+
+            Node<Person> nodePadre = new Node<Person>(padre);
+            Node<Person> nodeMadre = new Node<Person>(madre);
+
+            Node<Person> nodeHijo1 = new Node<Person>(hijo1);
+            Node<Person> nodeHijo2 = new Node<Person>(hijo2);
+            Node<Person> nodeHijo3 = new Node<Person>(hijo3);
+
+            nodeAbueloP.AddChildren(nodePadre);
+            nodeAbuelaP.AddChildren(nodePadre);
+
+            nodeAbueloM.AddChildren(nodeMadre);
+            nodeAbuelaM.AddChildren(nodeMadre);
+
+            nodePadre.AddChildren(nodeHijo1);
+            nodePadre.AddChildren(nodeHijo2);
+            nodePadre.AddChildren(nodeHijo3);
+
+            nodeMadre.AddChildren(nodeHijo1);
+            nodeMadre.AddChildren(nodeHijo2);
+            nodeMadre.AddChildren(nodeHijo3);
+           
+            
+
+
         }
     }
 }
